64587bb52d217e6e9309eca92fa2a0e246e6ecd1
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-06-06 08:11:02 +0200

    basic mobile view for home page

--- Frontend/my-college/src/App.js
+++ Frontend/my-college/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import Home from "../src/Pages/Home.js"
-import {Switch,Route} from "react-router-dom";
-import Courses from '../src/Pages/Courses';
-import Enrollments from "../src/Pages/Enrollments"
+import "./App.css";
+import Home from "../src/Pages/Home.js";
+import { Switch, Route } from "react-router-dom";
+import Courses from "../src/Pages/Courses";
+import Enrollments from "../src/Pages/Enrollments";
 import LogIn from "../src/Pages/LogIn";
-import StudentLogin from  "../src/Pages/StudentLogin";
+import StudentLogin from "../src/Pages/StudentLogin";
 import StaffLogin from "../src/Pages/StaffLogin";
-import CreateAccount from './Pages/CreateAccount';
+import CreateAccount from "./Pages/CreateAccount";
 import Applicants from "../src/Pages/Applicants";
 import StudentDashboard from "./Components/StudentDashboard";
 
@@ -14,18 +14,16 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route  path="/courses" component={Courses}/>
-        <Route  path="/enrollments" component={Enrollments}/>
-        <Route  path="/login" component={LogIn}/>
-        <Route  path="/Studentlogin" component={StudentLogin}/>
-        <Route  path="/Stafflogin" component={StaffLogin}/>
-        <Route  path="/courses" component={Courses}/>
-        <Route  path="/create-account" component={CreateAccount}/>
-        <Route  path="/applicants" component={Applicants}/>
-        <Route  path="/StudentDashboard" component={StudentDashboard}/>
-
-
+        <Route exact path="/" component={Home} />
+        <Route path="/courses" component={Courses} />
+        <Route path="/enrollments" component={Enrollments} />
+        <Route path="/login" component={LogIn} />
+        <Route path="/Studentlogin" component={StudentLogin} />
+        <Route path="/Stafflogin" component={StaffLogin} />
+        <Route path="/courses" component={Courses} />
+        <Route path="/create-account" component={CreateAccount} />
+        <Route path="/applicants" component={Applicants} />
+        <Route path="/StudentDashboard" component={StudentDashboard} />
       </Switch>
     </div>
   );
