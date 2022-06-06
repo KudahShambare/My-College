930c51e3c37aeecaaccc40e102a29882b38d3d47
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-06-06 08:11:27 +0200

    home mobile ui updated

--- Frontend/my-college/src/Pages/Home.js
+++ Frontend/my-college/src/Pages/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
   return (
     <>
       <div id="home">
+        {/*Desktop home page*/}
         <section className="homeTop">
           <Link to="/">
           <img src={Logo}  alt="logo" className="logo" height="400" /> 
@@ -21,9 +22,37 @@ const Home = () => {
        </section>
  
         </section>
-     
-    
+        {/* Mobile Home*/}
+     <section id="mobileHomeTop">
+
+     <Link to="/" >
+          <img src={Logo}  alt="logo" className="logo" height="400" style={{marginRight:"45%"}}/> 
+          </Link>
+  
+ <section  id="mobileLogInNav" >
+ <Link to="/login" >
+     <FaRegUserCircle  style={{fontSize:'3rem',Color:'black'}}/>
+    <h2 style={{fontSize:'1rem',marginTop:'0%'}}>Log In</h2>
+     </Link>
+       </section>
+       <h1>Welcome To The Fake College</h1>
+
+
+     </section>
+    {/* Home Buttons Big Screen*/}
         <section className="navButtons">
+       <Link to="/courses"> 
+       <button> View Our Courses </button>
+       </Link>
+       <Link to="/enrollments"> 
+       <button> Get Enrolled Today </button>
+
+       </Link>
+
+
+        </section>
+        {/* Home Buttons Mobile*/}
+        <section id="mobileNavButtons">
        <Link to="/courses"> 
        <button> View Our Courses </button>
        </Link>
