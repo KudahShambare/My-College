3e7341bf31d4ae60b5ce7ea0b1b930e87010fdb8
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2025-02-13 01:44:02 +0200

    updated routes

--- Frontend/my-college/src/App.js
+++ Frontend/my-college/src/App.js
@@ -3,7 +3,6 @@ import Home from "../src/Pages/Home.js";
 import { Routes, Route } from "react-router-dom";
 import Courses from "../src/Pages/Courses";
 import Enrollments from "../src/Pages/Enrollments";
-import LogIn from "../src/Pages/LogIn";
 import StudentLogin from "../src/Pages/StudentLogin";
 import StaffLogin from "../src/Pages/StaffLogin";
 import CreateAccount from "./Pages/CreateAccount";
@@ -19,11 +18,10 @@ function App() {
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/courses" element={<Courses/>} />
         <Route exact path="/enrollments" element={<Enrollments/>} />
-        <Route exact path="/join" element={<LogIn/>} />
-        <Route exact path="/Studentlogin" element={<StudentLogin/>} />
-        <Route exact path="/Stafflogin" element={<StaffLogin/>} />
-        <Route exact path="/create-account" element={<CreateAccount/>} />
-        <Route exact path="/applicants" element={<Applicants/>} />
+        <Route exact path="/account/student" element={<StudentLogin/>} />
+        <Route exact path="/account/staff" element={<StaffLogin/>} />
+        <Route exact path="/account/new" element={<CreateAccount/>} />
+        <Route exact path="/join" element={<Applicants/>} />
         <Route path="*" element={<NotFound/>} />
 
       </Routes>
